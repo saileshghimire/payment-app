2995df7a3d7b904b7cc115de3a42edd96d326f04
saileshghimire <saileshghimire1@gmail.com>
2024-06-03 14:01:33 +0545

    backend/index rotes setup mainRouter

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
 const express = require("express");
+const app = express();
+const mainRouter = require("./routes/index");
 
+app.use("api/v1",mainRouter);
 
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
