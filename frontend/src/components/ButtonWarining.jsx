e3b2d77f8a12303efc65d13d1e0997bb43936fcf
saileshghimire <saileshghimire1@gmail.com>
2024-06-06 23:31:25 +0545

    signup page finished

---
+++ frontend/src/components/ButtonWarining.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export function ButtonWarning({label, buttonText, to}) {
+    return(
+        <div className="flex justify-center text-sm py-2">
+            <div>
+                {label}
+            </div>
+            <Link className="pointer underline pl-1 cursor-pointer" to={to}>
+                {buttonText}
+            </Link>
+        </div>
+    )
+}
